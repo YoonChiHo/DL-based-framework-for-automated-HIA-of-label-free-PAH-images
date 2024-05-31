29b19878bf2e10831bbf5cfe72f759afa7b3e128
YoonChiHo <48755220+YoonChiHo@users.noreply.github.com>
2024-05-31 11:34:16 +0900

    Delete s2_Segmentation/code directory

--- s2_Segmentation/code/requirements.sh
+++
@@ -1,8 +0,0 @@
-pip install matplotlib
-pip install scikit-image==0.19.3
-pip install imutils
-pip install opencv-contrib-python
-apt-get update
-apt-get -y install libgl1-mesa-glx
-apt-get install -y libglib2.0-0
-pip install ttach
